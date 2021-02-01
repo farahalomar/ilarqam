5d270f40f6a5e90145524f66a8da5cffed4db738
farahalomar <53612293+farahalomar@users.noreply.github.com>
2021-02-01 15:02:00 +0300

    same thing different order

--- app/admin.py
+++ app/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Project, Country, City, Person
 
 admin.site.register(Project)
+admin.site.register(Person)
 admin.site.register(Country)
 admin.site.register(City)
-admin.site.register(Person)
